149b739e7d0ecce9105e1e45d91225e7f3f497aa
Jinlong0212 <jinlongluo0212@yahoo.com>
2025-03-24 21:59:35 -0700

    First commit for IT115 Class

---
+++ json.py
@@ -0,0 +1,14 @@
+#
+import json
+
+
+#Create the data dictionary
+
+data = {
+
+    'name': 'John Doe',
+    'age':30,
+    'city':'New York,NY',
+    'interests': ['golf','running','videogames'],
+    'is_student':False
+}
